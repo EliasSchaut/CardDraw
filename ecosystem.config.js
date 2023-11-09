83f0cb4bbabdfe176bb10d6554326b44c4a92aec
EliasSchaut <eschaut@outlook.de>
2023-11-09 20:20:17 +0100

    add pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,9 @@
-module.exports = {
-  apps: [
-    {
-      name: 'CardDraw',
-      port: '9092',
-      exec_mode: 'cluster',
-      instances: 'max',
-      script: './.output/server/index.mjs',
-    },
-  ],
-};
+export const apps = [
+  {
+    name: 'CardDraw',
+    port: '9092',
+    exec_mode: 'cluster',
+    instances: 'max',
+    script: './.output/server/index.mjs',
+  },
+];
